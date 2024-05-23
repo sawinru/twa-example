128e8a1c3a71df4870f689edc138c36a19583b48
Andrew Tkachenko <sawinru@gmail.com>
2024-05-23 17:58:16 +0300

    change manifest

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import App from './App.tsx'
 import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
-// this manifest is used temporarily for development purposes
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = 'https://sawinru.github.io/twa-example/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
